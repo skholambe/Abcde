e298c0b16c4466a5ae1115502f1cd5f04e2615bb
Shailesh <Shailesh@192.168.1.207>
2022-05-26 22:26:55 +0530

    First commit

---
+++ src/main/java/com/Git/ABCGit/Abcd.java
@@ -0,0 +1,16 @@
+package com.Git.ABCGit;
+
+public class Abcd {
+	
+	public void abc() {
+		System.out.println("Abc");
+	}
+	
+	public void abc1() {
+		System.out.println("Abc 1");
+	}
+
+	public void abc3() {
+		System.out.println("Abc 3");
+	}
+}
